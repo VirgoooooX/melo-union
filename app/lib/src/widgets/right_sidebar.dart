80c6499b95deab9efb54a5ebf671dbef6563aed1
VirgoooooX <152242900+VirgoooooX@users.noreply.github.com>
2026-06-29 18:25:55 +0800

    chore(ui): reset playback sidebar root

--- app/lib/src/widgets/right_sidebar.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-import '../bootstrap/demo_repository.dart';
-import '../design/melo_tokens.dart';
-
-part 'right_sidebar_now_playing.dart';
-part 'right_sidebar_queue.dart';
