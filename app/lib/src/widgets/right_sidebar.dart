77195933566cbb75cff7a3428af728f2abe795c5
VirgoooooX <152242900+VirgoooooX@users.noreply.github.com>
2026-06-29 18:26:03 +0800

    feat(ui): add clean playback sidebar root

---
+++ app/lib/src/widgets/right_sidebar.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../bootstrap/demo_repository.dart';
+import '../design/melo_tokens.dart';
+
+part 'right_sidebar_now_playing.dart';
+part 'queue_preview.dart';
