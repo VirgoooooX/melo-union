61c44a00519d34114adc8383a9fb0e220e3a76ba
VirgoooooX <152242900+VirgoooooX@users.noreply.github.com>
2026-06-29 18:23:41 +0800

    feat(ui): add full-width player bar shell

---
+++ app/lib/src/widgets/desktop_player_bar.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../bootstrap/demo_repository.dart';
+import '../design/melo_tokens.dart';
+
+part 'desktop_player_bar_view.dart';
