8cc79552a5b02b260c3e272d6337a8b88524d22c
VirgoooooX <152242900+VirgoooooX@users.noreply.github.com>
2026-06-29 18:44:12 +0800

    feat(settings): add product settings page root

---
+++ app/lib/src/features/settings/settings_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'package:provider_contract/provider_contract.dart';
+
+import '../../bootstrap/demo_repository.dart';
+import '../../design/melo_tokens.dart';
+import '../../presentation/app_destination.dart';
+
+part 'settings_page_view.dart';
+part 'settings_source_card.dart';
