488150896facf5b2be3d5f5a05adbae81a73d367
VirgoooooX <152242900+VirgoooooX@users.noreply.github.com>
2026-06-29 17:58:37 +0800

    feat: add provider tabs widget

---
+++ app/lib/src/widgets/provider_tabs.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+import '../design/melo_tokens.dart';
+
+class ProviderTabSpec {
+  const ProviderTabSpec({required this.id, required this.label});
+
+  final String id;
+  final String label;
+}
