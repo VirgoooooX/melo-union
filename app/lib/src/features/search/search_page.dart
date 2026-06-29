b4221c03c7c368f3b88c9dea42eb211f28fdf8ba
VirgoooooX <152242900+VirgoooooX@users.noreply.github.com>
2026-06-29 18:29:53 +0800

    feat(ui): add recommendation page root

---
+++ app/lib/src/features/search/search_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:provider_contract/provider_contract.dart';
+
+import '../../bootstrap/demo_repository.dart';
+import '../../design/melo_tokens.dart';
+import '../../widgets/provider_tabs.dart';
+
+part 'rec_view.dart';
+part 'rec_cards.dart';
