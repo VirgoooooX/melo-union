54d8f049b7551c49ae32b299b17c881007cacb22
VirgoooooX <152242900+VirgoooooX@users.noreply.github.com>
2026-06-29 18:43:35 +0800

    feat(search): replace recommendation alias with real search page

--- app/lib/src/features/search/search_page.dart
+++ app/lib/src/features/search/search_page.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:music_domain/music_domain.dart';
 import 'package:provider_contract/provider_contract.dart';
 
 import '../../bootstrap/demo_repository.dart';
 import '../../design/melo_tokens.dart';
 import '../../widgets/provider_tabs.dart';
 
-part 'rec_view.dart';
-part 'rec_cards.dart';
+part 'search_page_view.dart';
+part 'search_results.dart';
