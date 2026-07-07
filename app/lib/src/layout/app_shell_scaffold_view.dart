9672c010de79832e66ee4a517dce92c8901452d0
VirgoooooX <yuchen0876@163.com>
2026-07-07 12:19:01 +0800

    Add mobile navigation bar background color

--- app/lib/src/layout/app_shell_scaffold_view.dart
+++ app/lib/src/layout/app_shell_scaffold_view.dart
@@ -217,6 +217,7 @@ class _MobileShell extends StatelessWidget {
       ),
       bottomNavigationBar: NavigationBar(
         height: 64,
+        backgroundColor: meloShellChromeColor(0.76),
         selectedIndex: selected < 0 ? 0 : selected,
         onDestinationSelected: (index) => context.go(destinations[index].path),
         destinations: [
